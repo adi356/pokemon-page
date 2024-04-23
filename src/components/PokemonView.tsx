3ea4a06946916219645856fe3ed1521d609efe7d
Andrew <adi356@mail.umsl.edu>
2024-04-23 14:54:13 -0600

    cleanup

--- src/components/PokemonView.tsx
+++ src/components/PokemonView.tsx
@@ -1,6 +1,5 @@
 import { Pokemon } from "./PokemonType"
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
